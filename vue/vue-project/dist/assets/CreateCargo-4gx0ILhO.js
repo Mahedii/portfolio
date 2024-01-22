9baf7593af04f3e6a77cab55e70fcf3eec36ebb1
Md. Mahadi Islam <mahediihasan220@gmail.com>
2024-01-22 16:09:44 +0600

    :up: vue project build folder updated

---
+++ vue/vue-project/dist/assets/CreateCargo-4gx0ILhO.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-Rclj0A4P.js";const o={};function t(a,n){return c(),r("div")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ vue/vue-project/dist/assets/Toastr-bncgRa57.js
@@ -0,0 +1,1 @@
+import{i as m}from"./index-Rclj0A4P.js";const c={toastrMessage(o="Dummy Message",s="success",t=3e3,e="colored",a="zoom"){m(o,{autoClose:t,theme:e,type:s,transition:a})}};export{c};
